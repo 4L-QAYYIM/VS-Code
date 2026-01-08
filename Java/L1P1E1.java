e9bc5bbdc1279ec93ec9a3f93974a31de138b758
Al <zakirovalbert2050@gmail.com>
2026-01-08 07:45:23 +0500

    I completed L1P1E1

---
+++ Java/L1P1E1.java
@@ -0,0 +1,24 @@
+package Java;
+
+import java.util.Random;
+
+public class L1P1E1 {
+    public static void main(String[] args){
+        //Сформировать одномерный массив случайным образом. 
+        //Определить количество четных элементов массива, стоящих на четных местах.
+        
+        Random random = new Random();
+
+        int[] mass = new int[random.nextInt(10) + 5];
+        int count = 0;
+
+        for(int i = 0; i < mass.length; i++){
+            mass[i] = random.nextInt(50);
+            System.out.print(mass[i] + " ");
+            if((mass[i] % 2 == 0) && (i % 2 == 0)) count++;
+        }
+
+        System.out.println();
+        System.out.println(count);
+    }
+}
