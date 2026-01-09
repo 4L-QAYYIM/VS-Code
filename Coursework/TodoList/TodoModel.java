e9cb0d46dfd6b90c94dbf75b6ac9b4d4d759dc7f
Al <zakirovalbert2050@gmail.com>
2026-01-09 19:36:40 +0500

    I have completed a small project

--- Coursework/TodoList/TodoModel.java
+++ Coursework/TodoList/TodoModel.java
@@ -1,19 +1,24 @@
-package Coursework;
+package Coursework.TodoList;
 
 import java.util.ArrayList;
 
 public class TodoModel {
+    // List - to store tasks
     private ArrayList<Task> List = new ArrayList<>();
 
+    // Setter which adds the task
     public void addTask(Task task){
         List.add(task);
     }
-    public void deleteTask(int index){
-        List.remove(index);
+    // Method which deletes all tasks
+    public void deleteAll(){
+        List.clear();
     }
+    // Getter for getting all tasks
     public ArrayList<Task> getTasks(){
         return List;
     }
+    // Getter for getting a task that is located at the specified index 
     public Task getTask(int index){
         return List.get(index);
     }
