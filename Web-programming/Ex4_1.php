010b1f346810c167dfff7f014e773aeac9419c90
Al <zakirovalbert2050@gmail.com>
2026-01-11 13:07:45 +0500

    lab6_Ex1_T4

---
+++ Web-programming/Ex4_1.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Задание 3.1</title>
+</head>
+<body>
+    <h1>Задание 3.1</h1>
+    <form method="POST" action="">
+        <div>
+            <h2 id="input">Задача 1: Сделайте функцию, которая параметром будет принимать секунды, а возвращать количество суток, соответствующих этим секундам.</h2>
+            <input name="in1" type="number" required>
+        </div>
+        <button type="submit" name="send">Отправить</button>
+    </form>
+    <h2>Задача 2. Сделайте функцию, которая вернет текущий день недели словом.</h2>
+ 
+    <div id="result">
+        <?php
+
+        function getDay() {
+            $days = [
+                'Воскресенье', 
+                'Понедельник', 
+                'Вторник',     
+                'Среда',       
+                'Четверг',     
+                'Пятница',     
+                'Суббота'      
+            ];
+            $dayIt = date('w');
+
+            return $days[$dayIt];
+        }
+
+        function getCountOfDay($num){
+            return (($num/60)/60)/24;
+        }
+
+        if (isset($_POST['send'])) {
+            $in1 = $_POST['in1'];
+
+            echo "<h3>Ответ:</h3>";
+
+            echo "Задача 1. В $in1 секундах содержится суток: " . round(getCountOfDay($in1), 2);
+
+            echo "<p>Задача 2:</p>";
+            echo "Сегодня: " . getDay();
+        }
+        ?>
+    </div>
+</body>
+</html>
